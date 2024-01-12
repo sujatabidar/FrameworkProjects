49d128e0de50d0e3d590219ae72633d10ef231ef
sujatabidar <sujatabidar039@gmail.com>
2024-01-12 19:12:35 +0530

    new runner file added

--- src/test/java/Runner/googleRunner.java
+++ src/test/java/Runner/googleRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
     features = "src/test/resources/features/Login.feature", // Path to your feature files
     glue = {"stepdefinitions.google"}, // Package where your step definitions are located
     tags = "@smoke", // Optional: Only run scenarios with this tag
-    monochrome = true,
+    monochrome = false,
    plugin = {"pretty", "html:target/cucumberReports/cucumberReport.html"} // Optional: Define plugins
 )
 
